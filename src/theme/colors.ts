7fac116ac24b04bf0232632f8b1a946c64e61216
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2022-01-29 18:27:25 +0200

    added vars

---
+++ src/theme/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  green: {
+    '100': '#4B613D',
+    '500': '#31C12B',
+  },
+  black: {
+    '100': '#000000',
+    '200': '#2A3423',
+  },
+  white: {
+    '100': '#FFFFFF',
+  },
+};
+
+export default colors;
